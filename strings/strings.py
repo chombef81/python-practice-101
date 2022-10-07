c4cf5a4515a7bf52d7e220dc7234d89776aa5ea9
Felix Chombe <felixchombe@Felixs-MacBook-Pro.local>
2022-10-07 22:35:38 +0100

    strings

---
+++ strings/strings.py
@@ -0,0 +1,22 @@
+my_string = 'Hello World'
+print(my_string)
+
+# using ' in a string
+my_other_string = 'I\'m a programmer'
+print(my_other_string)
+
+# multi line string
+sentance = """I rely enjoy doing this
+but I need to start doing 
+more projects!!"""
+print(sentance)
+
+# getting elements of the string
+substring = my_string[1:7]
+print(substring)
+
+# using third [:::]
+subs = my_string[::2]
+print(subs)
+subs2 = my_string[::-1]
+print(subs2)
